a05178f75f19cb4639d2842b8d4b1ee0ac03f3be
Orland Contreras <orland102@gmail.com>
2018-11-28 19:22:45 -0500

    Change Keys

--- place/place.js
+++ place/place.js
@@ -6,7 +6,7 @@ const getPlaceLatLng = async (address) => {
 
     let encodeUrl = encodeURI( address );
 
-    let res = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ encodeUrl }&key=AIzaSyDyJPPlnIMOLp20Ef1LlTong8rYdTnaTXM`);
+    let res = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ encodeUrl }&key=YOUR_KEY`);
     
     if (res.data.status === 'ZERO_RESULTS') {
         throw new Error(`There is no result for the city ${ address }`);
--- weather/weather.js
+++ weather/weather.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const getWeather = async (lat, lng) => {
-    let res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=b678d5dc8cf5595c32c9bd8a3127b9b5`);
+    let res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=YOUR_KEY`);
 
     return res.data.main.temp;
 }
